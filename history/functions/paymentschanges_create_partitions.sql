ca9221f643d5378322d56bbb612877bc457bdd67
Jurieno <94364464+Jurieno@users.noreply.github.com>
2023-10-24 17:12:52 +0700

    codestyle

--- history/functions/paymentschanges_create_partitions.sql
+++ history/functions/paymentschanges_create_partitions.sql
@@ -4,21 +4,21 @@ CREATE OR REPLACE FUNCTION history.paymentschanges_create_partition() RETURNS js
 AS
 $$
 DECLARE
-    _dt DATE := date_trunc('YEAR', now());
-    _i DATE;
+    _dt                   DATE := date_trunc('YEAR', now());
+    _i                    DATE;
     _partition_table_name TEXT;
 BEGIN
     FOR _i IN
         SELECT d FROM generate_series(_dt, _dt + '11 month'::INTERVAL, '1 month'::INTERVAL) d
-    LOOP
-        _partition_table_name := FORMAT('paymentschanges_%s', REGEXP_REPLACE(_i::TEXT, '-', '_', 'g')::TEXT);
-        IF (TO_REGCLASS(_partition_table_name::TEXT) ISNULL) THEN
-            EXECUTE FORMAT(
-              'CREATE TABLE history.%I PARTITION OF history.paymentschanges'
-              '     FOR VALUES FROM (%L) TO (%L);'
-              , _partition_table_name, _i, _i + '1 month'::INTERVAL);
-        END IF;
-    END LOOP;
+        LOOP
+            _partition_table_name := FORMAT('paymentschanges_%s', REGEXP_REPLACE(_i::TEXT, '-', '_', 'g')::TEXT);
+            IF (TO_REGCLASS(_partition_table_name::TEXT) ISNULL) THEN
+                EXECUTE FORMAT(
+                        'CREATE TABLE history.%I PARTITION OF history.paymentschanges'
+                            '     FOR VALUES FROM (%L) TO (%L);'
+                    , _partition_table_name, _i, _i + '1 month'::INTERVAL);
+            END IF;
+        END LOOP;
 
     RETURN jsonb_build_object('data', NULL);
 END
